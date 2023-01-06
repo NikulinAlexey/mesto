0e099f8a5366716465541d68c65ceb5f835b3f8a
Aleksey Nikulin <donaldduckisnotdead@gmail.com>
2023-01-06 16:50:47 +0300

    feat: Научить страницу включать и выключать попап

--- script.js
+++ script.js
@@ -11,5 +11,5 @@ function popUpOff () {
     popUp.style = 'display: none';
 }
 editButton.addEventListener('click', popUpOn);
-closeIcon.addEventListener('click', popUpOff)
+closeIcon.addEventListener('click', popUpOff);
 
