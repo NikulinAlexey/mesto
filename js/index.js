69d372609e9fb2e1d2ff2719a357e97a429c0c69
Aleksey Nikulin <donaldduckisnotdead@gmail.com>
2023-01-09 17:14:04 +0300

    refactor: Перебрать весь код файла index.js, восстановить функционал

---
+++ js/index.js
@@ -0,0 +1,34 @@
+// Реализация возможности открытия-закрытия pop-up
+let editButton = document.querySelector('.profile__edit-button');
+let popUp = document.querySelector('.popup');
+let closeIcon = document.querySelector('.popup__close-icon');
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_job');
+let nameProfile = document.querySelector('.profile__name');
+let jobProfile = document.querySelector('.profile__job');
+let formElement = document.querySelector('.popup__form');
+
+function openPopup () {
+    popUp.classList.add('popup_opened');
+    nameInput.value = nameProfile.textContent;
+    jobInput.value = jobProfile.textContent;
+}
+
+function closePopup () {
+    popUp.classList.remove('popup_opened');
+}
+
+function handleFormSubmit (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+    
+    nameProfile.textContent = `${nameInput.value}`;
+    jobProfile.textContent = `${jobInput.value}`;
+   
+    closePopup();
+}
+
+formElement.addEventListener('submit', handleFormSubmit)
+editButton.addEventListener('click', openPopup);
+closeIcon.addEventListener('click', closePopup);
