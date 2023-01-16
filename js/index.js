96c365b6eedac6ed4e96cd94947cb4d7347fa879
Aleksey Nikulin <donaldduckisnotdead@gmail.com>
2023-01-16 23:27:53 +0300

    feature: Сделать загрузку контента карточек из коробки при загрузке

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-// Реализация возможности открытия-закрытия pop-up
 let editButton = document.querySelector('.profile__edit-button');
+let addButton = document.querySelector('.profile__add-button');
 let popUp = document.querySelector('.popup');
 let closeIcon = document.querySelector('.popup__close-icon');
 let nameInput = document.querySelector('.popup__input_type_name');
@@ -7,8 +7,12 @@ let jobInput = document.querySelector('.popup__input_type_job');
 let nameProfile = document.querySelector('.profile__name');
 let jobProfile = document.querySelector('.profile__job');
 let formElement = document.querySelector('.popup__form');
-let element = document.querySelector('.element')
-let likeButton = element.querySelector('.element__like');
+let element = document.querySelector('.element');
+let elementsImage = document.querySelectorAll('.element__image');
+let elementsTitle = document.querySelectorAll('.element__title');
+const likeButton = element.querySelector('.element__like');
+const cards = document.querySelectorAll('.element');
+
 const initialCards = [
     {
       name: 'Архыз',
@@ -34,7 +38,9 @@ const initialCards = [
       name: 'Байкал',
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
-  ]; 
+  ];
+
+  startingCards();
 
 function openPopup () {
     popUp.classList.add('popup_opened');
@@ -53,6 +59,15 @@ function handleFormSubmit (evt) {
     jobProfile.textContent = `${jobInput.value}`;
    
     closePopup();
+}
+
+let elementsArray = [];
+
+function startingCards () {
+    for (let i = 0; i < initialCards.length; i++) {
+      elementsTitle[i].textContent = `${initialCards[i].name}`;
+      elementsImage[i].setAttribute('src', `${initialCards[i].link}`);
+    }
 }
 
 likeButton.addEventListener('click', function (evt) {
